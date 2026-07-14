a66e54a7fa62a36b8f826ba4e3faec7c91cff57f
neehanazer <neehanaz226@gmail.com>
2026-07-14 07:57:52 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AddCar from './components/AddCar'
+import ViewCar from './components/ViewCar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,9 +16,7 @@ function App() {
     <Routes>
   {/* <Route index element={<StepOne />} /> */}
   <Route path="/" element={<AddCar />} />
-  {/* <Route path="search" element={<SearchHb />} />
-  <Route path="delete" element={<DeleteHb />} />
-  <Route path="view" element={<ViewHb />} /> */}
+  <Route path="view" element={<ViewCar />} />
 </Routes>
      </BrowserRouter>
     </>
